f0b8557d8adde6c5394ba1358185487fa344dbec
ericheiser <ericheiser@gmail.com>
2018-03-29 15:45:08 -0600

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+node.default['haproxy']['members'] = [
+  {
+    "hostname" => "ec2-54-145-214-119.compute-1.amazonaws.com",
+    "ipaddress" => "54.145.214.119",
+    "port" => 80,
+    "ssl_port" => 80
+  }]
+include_recipe 'haproxy::manual'
